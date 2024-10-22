8ece7a9fd15e530d0a1c38c0e7556eb98e74bfbd
MohmmadAladarbeh <mohmadadarbeh7@gmail.com>
2024-10-22 12:47:42 +0000

    add

---
+++ ProductPortal/ProductPortal/src/main/java/org/example/productportal/config/JSFConfig.java
@@ -0,0 +1,11 @@
+package org.example.productportal.config;
+
+import org.springframework.boot.web.servlet.ServletRegistrationBean;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.faces.webapp.FacesServlet;
+
+@Configuration
+public class JSFConfig {
+}
